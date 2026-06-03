Fix astronomically high losses: reduce lr to 0.001, add LayerNorm to residuals, remove double softmax on target_policy
Co-authored-by: multica-agent <github@multica.ai>

Models/MuZero_torch_model.py
@@ -351,6 +351,10 @@ class PredNetwork(torch.nn.Module):
         self.policy_dense3 = torch.nn.Linear(layer_size, layer_size)
         self.policy_dense4 = torch.nn.Linear(layer_size, config.ACTION_CONCAT_SIZE)  # 3 action dims * 37 max actions per dim
         self.softmax = torch.nn.Softmax(dim=1)
+        self.ln1 = torch.nn.LayerNorm(layer_size)
+        self.ln2 = torch.nn.LayerNorm(layer_size)
+        self.ln3 = torch.nn.LayerNorm(layer_size)
+        self.ln4 = torch.nn.LayerNorm(layer_size)
 
     def forward(self, x):
         # x = self.resnet(x)
@@ -373,10 +377,10 @@ class PredNetwork(torch.nn.Module):
         
         # x = torch.squeeze(x)
         x = self.relu(self.dense1(x))
-        x = self.relu(self.dense2(x)) + x
-        x = self.relu(self.dense3(x)) + x
-        x = self.relu(self.dense4(x)) + x
-        x = self.relu(self.dense5(x)) + x
+        x = self.ln1(self.relu(self.dense2(x)) + x)
+        x = self.ln2(self.relu(self.dense3(x)) + x)
+        x = self.ln3(self.relu(self.dense4(x)) + x)
+        x = self.ln4(self.relu(self.dense5(x)) + x)
         x = self.dense6(x)
 
         policy = self.relu(self.policy_dense1(x)) + x
@@ -412,6 +416,10 @@ class RepNetwork(torch.nn.Module):
         self.dense4 = torch.nn.Linear(hidden, hidden)
         self.dense5 = torch.nn.Linear(hidden, hidden)
         self.dense6 = torch.nn.Linear(hidden, hidden)
+        self.ln1 = torch.nn.LayerNorm(hidden)
+        self.ln2 = torch.nn.LayerNorm(hidden)
+        self.ln3 = torch.nn.LayerNorm(hidden)
+        self.ln4 = torch.nn.LayerNorm(hidden)
 
     def forward(self, x):
         # x = torch.squeeze(x, dim=2)
@@ -423,10 +431,10 @@ class RepNetwork(torch.nn.Module):
 
         # x = torch.squeeze(x)
         x = self.relu(self.dense1(x))
-        x = self.relu(self.dense2(x)) + x
-        x = self.relu(self.dense3(x)) + x
-        x = self.relu(self.dense4(x)) + x
-        x = self.relu(self.dense5(x)) + x
+        x = self.ln1(self.relu(self.dense2(x)) + x)
+        x = self.ln2(self.relu(self.dense3(x)) + x)
+        x = self.ln3(self.relu(self.dense4(x)) + x)
+        x = self.ln4(self.relu(self.dense5(x)) + x)
         x = self.dense6(x)
 
         return x

Models/MuZero_torch_trainer.py
@@ -104,7 +104,7 @@ class Trainer(object):
 
             policy_logits_flat = policy_logits.view(policy_logits.shape[0], -1)
             
-            target_policy_normalized = torch.nn.functional.softmax(target_policy[:, tstep], dim=-1)
+            target_policy_normalized = target_policy[:, tstep]
             policy_log_probs = torch.nn.functional.log_softmax(policy_logits_flat, dim=-1)
             policy_loss += kl_loss_fn(policy_log_probs, target_policy_normalized)
 